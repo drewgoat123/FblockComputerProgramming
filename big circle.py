8b8fae1607f759634b0c43ef5fb97238c344953c
drewgoat123 <drew.davine@eaglebrook.org>
2022-01-21 11:11:18 -0500

    jan21 classwork

---
+++ big circle.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+color("blue", "red")
+
+begin_fill()
+forward(90)
+circle(144)
+forward(90)
+circle(144)
+forward(90)
+circle(144)
+forward(90)
+circle(144)
+forward(90)
+end_fill()
+
+
+
+done()
--- jan11.py
+++ jan11.py
@@ -1,7 +1,10 @@
-def jan11a(x):
-	return x
+def jan11a(d):
+	return d * 4+6
 
 
 print( jan11a( 6 ))
 print( jan11a( 7 ))
 print( jan11a( 8 ))
+print()
+for d in range (10):
+	print (jan11a( d ))
