9bd9adab9bc138fc6e956791fe649a6e552af378
drewgoat123 <drew.davine@eaglebrook.org>
2022-01-24 13:59:49 -0500

    jan 24 class

---
+++ turtlename.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+color("blue")
+left(90)
+circle(100, 90, None)
+left(90)
+forward(200)
+left(90)
+circle(100, 90, None)
+penup()
+right(90)
+forward(90)
+pendown()
+forward(25)
+circle(35)
+ 
+
+done()
